cef70afe9afd142b9dbe7ff383b1bdbc899baf6b
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2020-06-18 03:09:34 +0500

    new bugs fix

--- src/views/Head.vue
+++ src/views/Head.vue
@@ -14,6 +14,129 @@ export default {
   components: {
     Headsection
   },
+
+  mounted(){
+
+        // Category Owl Carousel
+    const objowlcarousel = $('.owl-carousel-category');
+    if (objowlcarousel.length > 0) {
+        objowlcarousel.owlCarousel({
+            responsive: {
+                0: {
+                    items: 3,
+                },
+                600: {
+                    items: 4,
+                },
+                1000: {
+                    items: 6,
+                },
+                1200: {
+                    items: 8,
+                },
+            },
+            loop: true,
+            lazyLoad: true,
+            autoplay: true,
+            dots: false,
+            autoplaySpeed: 1000,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            nav: true,
+            navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+        });
+    }
+
+      // Homepage Owl Carousel  
+    var fiveobjowlcarousel = $(".owl-carousel-four");
+    if (fiveobjowlcarousel.length > 0) {
+        fiveobjowlcarousel.owlCarousel({
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 2,
+                },
+                1000: {
+                    items: 4,
+                },
+                1200: {
+                    items: 4,
+                },
+            },
+
+            lazyLoad: true,
+            pagination: false,
+            loop: true,
+            dots: false,
+            autoPlay: 2000,
+            nav: true,
+            stopOnHover: true,
+            navText: ["<i class='icofont-thin-left'></i>", "<i class='icofont-thin-right'></i>"]
+        });
+    }
+
+    // Owl Carousel Five
+    var fiveobjowlcarousel = $(".owl-carousel-five");
+    if (fiveobjowlcarousel.length > 0) {
+        fiveobjowlcarousel.owlCarousel({
+            responsive: {
+                0: {
+                    items: 2,
+                },
+                600: {
+                    items: 3,
+                },
+                1000: {
+                    items: 4,
+                },
+                1200: {
+                    items: 5,
+                },
+            },
+            lazyLoad: true,
+            pagination: false,
+            loop: true,
+            dots: false,
+            autoPlay: 2000,
+            nav: true,
+            stopOnHover: true,
+            navText: ["<i class='icofont-thin-left'></i>", "<i class='icofont-thin-right'></i>"]
+        });
+    }
+
+    // Homepage Ad Owl Carousel
+    const mainslider = $('.homepage-ad');
+    if (mainslider.length > 0) {
+        mainslider.owlCarousel({
+            responsive: {
+                0: {
+                    items: 2,
+                },
+                764: {
+                    items: 2,
+                },
+                765: {
+                    items: 1,
+                },
+                1200: {
+                    items: 1,
+                },
+            },
+            lazyLoad: true,
+            loop: true,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            dots: false,
+            autoplayTimeout: 2000,
+            nav: true,
+            navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+            autoplayHoverPause: true,
+        });
+    }
+
+  },
  
 }
 </script>
